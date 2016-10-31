9e57f04f3d3673578fe6a54e65b7c38b9e2f9fd2
ppzz <penn.chn@gmail.com>
2016-10-31 13:56:10 +0800

    zp@20: pass test 1

--- 20.pre-pos/practices/section_1/practice_1/practice.js
+++ 20.pre-pos/practices/section_1/practice_1/practice.js
@@ -1,5 +1,13 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var temp = [];
+  collection_a.forEach(function (item) {
+    if (collection_b.indexOf(item)!== -1) {
+      temp.push(item);
+    }
+  });
+
+  return temp;
 }
 
 module.exports = collect_same_elements;
